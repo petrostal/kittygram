31d60d25109d08c037130e95332261ce1852031a
petrostal <topchienko.peter@gmail.com>
2023-07-06 15:33:24 +0300

    lowlevel class api views

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import APICat, APICatDetail
 
 urlpatterns = [
-   path('cats/', cat_list),
+   path('cats/', APICat.as_view()),
+   path('cats/<int:pk>/', APICatDetail.as_view()),
 ]
-
-
